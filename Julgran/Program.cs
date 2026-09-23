2821c3fad2bfaab4555de4ccf136156dfdc52aef
Kristin Törnberg <tornbergkristin@gmail.com>
2026-09-23 19:14:43 +0200

    Endast blinkande julgran

--- Julgran/Program.cs
+++ Julgran/Program.cs
@@ -6,67 +6,6 @@ namespace Julgran
     {
         static void Main(string[] args)
         {
-            //Console.Write("Ange antal rader: ");
-            //int antal = int.Parse(Console.ReadLine());
-
-            //for (int i = 0; i < antal; i++)
-            //{
-            //    // Mellanslag före stjärnorna
-            //    for (int j = 0; j < antal - i - 1; j++)
-            //    {
-            //        Console.Write(" ");
-            //    }
-
-            //    // Stjärnor
-            //    for (int j = 0; j < 2 * i + 1; j++)
-            //    {
-            //        Console.Write("*");
-            //    }
-
-            //    Console.WriteLine();
-            //}
-
-            //Console.Write("Ange granens höjd: ");
-            //int höjd = int.Parse(Console.ReadLine());
-
-            //int bredd = höjd * 2 - 1;
-            //Random slump = new Random();
-            //// Rita granens delar
-            //for (int del = 1; del <= 3; del++)
-            //{
-            //    for (int rad = 0; rad < del + 1; rad++)
-            //    {
-            //        int stjärnor = rad * 2 + 1;
-            //        int mellanslag = (bredd - stjärnor) / 2;
-
-            //        for (int i = 0; i < mellanslag; i++)
-            //            Console.Write(" ");
-
-            //        for (int i = 0; i < stjärnor; i++)
-            //        {
-            //            // Första raden ska alltid vara en stjärna
-            //            if (del == 1 && rad == 0)
-            //            {
-            //                Console.Write("*");
-            //            }
-            //            else
-            //            {
-            //                // Cirka 20% chans för en julkula
-            //                if (slump.Next(5) == 1)
-            //                {
-            //                    Console.Write("o");
-            //                }
-            //                else
-            //                {
-            //                    Console.Write("*");
-            //                }
-            //            }
-            //        }
-
-            //        Console.WriteLine();
-            //    }
-            //}
-
             Random random = new Random();
 
             int antalGranar = 4;
@@ -140,21 +79,6 @@ namespace Julgran
 
                     Console.WriteLine();
                 }
-
-                //// Rita stammar
-                //for (int gran = 0; gran < antalGranar; gran++)
-                //{
-                //    int höjd = höjder[gran];
-
-                //    for (int i = 0; i < höjd - 1; i++)
-                //        Console.Write(" ");
-
-                //    Console.ForegroundColor = ConsoleColor.DarkYellow;
-                //    Console.Write("|");
-                //    Console.ResetColor();
-
-                //    Console.Write(new string(' ', höjd + 3));
-                //}
 
                 Console.WriteLine();
 
