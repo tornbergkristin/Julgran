89742862b5bcc06b7fd1d55691b4a52da00d3b64
Kristin Törnberg <tornbergkristin@gmail.com>
2026-09-23 14:14:16 +0200

    Add project files.

---
+++ Julgran/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Julgran
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ange ett tal: ");
+            if (int.TryParse(Console.ReadLine(), out int antal))
+            {
+                for (int rad = 1; rad <= antal; rad++)
+                {
+                    Console.WriteLine("*");
+
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
